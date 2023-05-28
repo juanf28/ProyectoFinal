04ebc6c86dfc3fff569e8d103c45a7d4317ceb0d
juanf28 <juan-felipe-00@hotmail.com>
2023-05-28 09:51:27 -0500

    implementacion sesion

---
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/implementacion/SesionServicioImpl.java
@@ -0,0 +1,41 @@
+package co.edu.uniquindio.proyecto.servicios.implementacion;
+
+
+import co.edu.uniquindio.proyecto.dto.SesionDTO;
+import co.edu.uniquindio.proyecto.dto.TokenDTO;
+import co.edu.uniquindio.proyecto.seguridad.modelo.UserDetailsImpl;
+import co.edu.uniquindio.proyecto.seguridad.servicios.JwtService;
+import co.edu.uniquindio.proyecto.servicios.interfaces.SesionServicio;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+class SesionServicioImpl implements SesionServicio {
+    @Autowired
+    private final AuthenticationManager authenticationManager;
+    @Autowired
+    private final JwtService jwtService;
+
+    @Override
+    public TokenDTO login(SesionDTO sesionDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        sesionDTO.getEmail(),
+                        sesionDTO.getPassword())
+        );
+        UserDetails user = (UserDetailsImpl) authentication.getPrincipal();
+        String jwtToken = jwtService.generateToken(user);
+        return new TokenDTO(jwtToken);
+    }
+
+    @Override
+    public void logout(int codigoUsuario) {
+
+    }
+}
